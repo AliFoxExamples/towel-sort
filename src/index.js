620626e3ef11c0ad01af5702d1d652ff737b2b7d
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-10 18:35:12 +0300

    refactor: change algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return !matrix || !(matrix.length) ? []:matrix.toString().split(",").map(el=>+el);
+  return !matrix || !(matrix.length) ? [] : matrix.map((el, i, arr)=>{
+  
+      return i&1? el.reverse():el;
+
+  }).toString().split(',').map(el=>+el);
 }
