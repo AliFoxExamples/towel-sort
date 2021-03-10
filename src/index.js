6ea7b4a00eb77fc0b363632ab0f6c7fa880e29d0
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-10 18:09:37 +0300

    fix: add exceptions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return matrix.toString().split(",").map(el=>+el);
+  return !matrix || !(matrix.length) ? []:matrix.toString().split(",").map(el=>+el);
 }
